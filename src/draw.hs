0550ebf4ebd4c8ca2416b924690f48d2a1f4914e
chalkos <chalkos@chalkos.net>
2013-10-17 10:37:50 +0100

    código limpo e com comentários, já lê scores, falta tile

--- src/draw.hs
+++ src/draw.hs
@@ -43,12 +43,25 @@ main = do entrada <- getContents
 processa :: Element -> String
 processa e = show (processaBoard e)
 
--- auxiliar
+-- Obtém apenas os Element de um conjunto de Content
 getElemsFromContent :: [Content] -> [Element]
 getElemsFromContent [] = []
 getElemsFromContent ((Elem e):t) = e:(getElemsFromContent t)
 getElemsFromContent (_:t) = getElemsFromContent t
 
+-- encontra o valor inteiro para determinada chave num conjunto de atributos
+getAttrValueInt :: String -> [Attr] -> Int
+--getAttrValueInt [] = ????
+getAttrValueInt key ((Attr {attrKey=(QName {qName=attrK}), attrVal=attrV}):t) = if attrK == key then (read attrV)
+                                                                                else getAttrValueInt key t
+
+-- encontra o valor char para determinada chave num conjunto de atributos
+getAttrValueChar :: String -> [Attr] -> Char
+--getAttrValueChar [] = ????
+getAttrValueChar key ((Attr {attrKey=(QName {qName=attrK}), attrVal=attrV}):t) = if attrK == key then (toUpper (head attrV))
+                                                                                else getAttrValueChar key t
+
+-- board
 processaBoard :: Element -> Board
 processaBoard (Element {elName=(QName {qName="board"}), elAttribs=((Attr {attrKey=(QName {qName="players"}), attrVal=n}):[]), elContent=children} ) = 
     Board { b_players = read n
@@ -57,25 +70,28 @@ processaBoard (Element {elName=(QName {qName="board"}), elAttribs=((Attr {attrKe
           , b_next    = processaNext (getElemsFromContent children)
           }
 
+-- terrain
 processaTerrain :: [Element] -> [Tile]
 processaTerrain _ = []
 
+-- encontra a tag scores
 processaScores :: [Element] -> [Score]
-processaScores _ = []
+processaScores ((Element {elName=(QName {qName="scores"}), elContent=children} ):_) = processaScore (getElemsFromContent children)
+processaScores (_:t) = processaScores t
 
+-- encontra as tags score
+processaScore :: [Element] -> [Score]
+processaScore [] = []
+processaScore ((Element {elName=(QName {qName="score"}), elAttribs=attr} ):t) =
+        (Score {s_player=(getAttrValueInt "player" attr), s_score=(getAttrValueInt "score" attr)}):(processaScore t)
+-- processaScore (_:t) = processaScore t -- não deve ser preciso a não ser que o xml esteja mal formatado
+
+-- encontra a tag next
 -- se não encontrar a tag next dá erro, como é suposto
 processaNext :: [Element] -> Next
 processaNext ((Element {elName=(QName {qName="next"}), elAttribs=attr} ):_) =
-        Next {n_player=(processaNextAttr_NPlayer attr), n_tile=(processaNextAttr_NTile attr)}
+        Next {n_player=(getAttrValueInt "player" attr), n_tile=(getAttrValueChar "tile" attr)}
 processaNext (_:t) = processaNext t
-
-processaNextAttr_NPlayer :: [Attr] -> Int
-processaNextAttr_NPlayer ((Attr {attrKey=(QName {qName="player"}), attrVal=n}):_) = read n
-processaNextAttr_NPlayer (_:t) = processaNextAttr_NPlayer t
-
-processaNextAttr_NTile :: [Attr] -> Char
-processaNextAttr_NTile ((Attr {attrKey=(QName {qName="tile"}), attrVal=(c:_)}):_) = toUpper c
-processaNextAttr_NTile (_:t) = processaNextAttr_NTile t
 
 
 --Element  
